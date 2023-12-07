97d511f26cc05d1db6275d5d6f038ad47dccf60e
Bruno Mello <bruno.barros.mello@gmail.com>
2023-12-07 19:40:04 -0300

    feat: tagger

--- src/lib.rs
+++ src/lib.rs
@@ -11,9 +11,48 @@ impl TextTagger {
         }
     }
 
-    fn tag(&mut self) {}
+    pub fn tag(&mut self) {
+        self.input_text.split("\n").for_each(|line| {
+            let trimmed = line.trim();
 
-    fn get_output_text(&self) -> String {
+            if trimmed.is_empty() {
+                return;
+            }
+
+            self.output_text.push_str("<p>");
+            self.output_text.push_str(trimmed);
+            self.output_text.push_str("</p>");
+            self.output_text.push_str("\n");
+        })
+    }
+
+    pub fn get_output_text(&self) -> String {
         self.output_text.clone()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_tag() {
+        let mut tagger = TextTagger::new("banana");
+        tagger.tag();
+        assert_eq!(tagger.get_output_text(), "<p>banana</p>\n");
+    }
+
+    #[test]
+    fn test_tag_multiple_lines() {
+        let mut tagger = TextTagger::new("\n\nuva\n\nbanana\n\n");
+        tagger.tag();
+        assert_eq!("<p>uva</p>\n<p>banana</p>\n", tagger.get_output_text());
+    }
+
+    #[test]
+    fn test_output() {
+        let mut tagger = TextTagger::new("banana");
+        tagger.tag();
+        assert_eq!(tagger.get_output_text(), "<p>banana</p>\n");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
     let mut tagger = text_tagger::TextTagger::new("banana");
+
+    tagger.tag();
 }
