bc3ba2419027266785ff6cc0d94ae97dc6be3238
Bruno Mello <bruno.barros.mello@gmail.com>
2023-12-08 17:34:48 -0300

    wip: parsing csv

--- src/csv_parser.rs
+++ src/csv_parser.rs
@@ -1,22 +1,49 @@
-use std::fs::File;
+use std::{error::Error, fs::File};
 
-use csv::Reader;
+use csv::{Reader, Writer};
 
-use crate::util::exit_with_message;
-
+#[derive(Debug)]
 pub struct CSVParser {
     reader: Reader<File>,
+    writer: Writer<File>,
+}
+
+#[derive(Debug, serde::Deserialize)]
+struct CSVRow {
+    #[serde(rename = "_IDSKU (Não alterável)")]
+    id: String,
+    #[serde(rename = "descrição gerada pela ia")]
+    value: String,
 }
 
 impl CSVParser {
-    pub fn new(file_path: impl Into<String>) -> CSVParser {
-        let file_path: String = file_path.into();
+    pub fn new(
+        input_file_path: impl Into<String>,
+        output_file_path: impl Into<String>,
+    ) -> Result<CSVParser, String> {
+        let input_file_path: String = input_file_path.into();
+        let output_file_path: String = output_file_path.into();
 
-        let rdr = match csv::Reader::from_path(&file_path) {
+        let reader = match csv::Reader::from_path(&input_file_path) {
             Ok(rdr) => rdr,
-            Err(err) => exit_with_message(format!("Ops! não consegui abrir o arquivo {}. Tem certeza que você está rodando o programa no mesmo diretório deste arquivo? Mais detalhes:\n{}", file_path, err), 1),
+            Err(err) => return Err(format!("Ops! não consegui abrir o arquivo {}. Tem certeza que você está rodando o programa no mesmo diretório deste arquivo? Mais detalhes:\n{}", input_file_path, err)) 
         };
 
-        CSVParser { reader: rdr }
+        let writer = match csv::Writer::from_path(&output_file_path) {
+            Ok(writer) => writer,
+            Err(err) => return Err(format!("Ops! Estou tentando salvar as informações no arquivo \"{}\" mas estou tendo uns problemas =/ Mais detalhes:\n{}", output_file_path, err)) 
+        };
+
+        Ok(CSVParser { reader, writer })
+    }
+
+    pub fn parse(&mut self) -> Result<(), Box<dyn Error>> {
+        for result in self.reader.deserialize() {
+            let csv_row: CSVRow = result?;
+            println!("{:?}", csv_row);
+            break;
+        }
+
+        Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,22 @@ mod csv_parser;
 mod tagger;
 mod util;
 
-use std::fs;
+use std::{error::Error, fs};
 
-const INPUT_FILE: &str = "input.txt";
-const OUTPUT_FILE: &str = "output.txt";
+use util::exit_with_message;
 
-fn main() {
-    let _parser = csv_parser::CSVParser::new("input.csv");
+const INPUT_FILE: &str = "input.csv";
+const OUTPUT_FILE: &str = "output.csv";
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut parser = match csv_parser::CSVParser::new(INPUT_FILE, OUTPUT_FILE) {
+        Ok(parser) => parser,
+        Err(msg) => exit_with_message(msg, 1),
+    };
+
+    parser.parse()?;
+
+    Ok(())
 
     // let content = match get_content() {
     //     Ok(content) => content,
@@ -22,16 +31,4 @@ fn main() {
     //     Ok(_) => exit_with_message("Arquivo de saída gerado com sucesso!", 0),
     //     Err(err) => exit_with_message(err, 1),
     // }
-}
-
-fn get_content() -> Result<String, String> {
-    let content = fs::read(INPUT_FILE).map_err(|_| "Epa! Não consegui ler o arquivo de entrada. Tem certeza que você está rodando o programa no mesmo diretório que arquivo \"input.txt\"?")?;
-
-    String::from_utf8(content).map_err(|e| format!(
-        "Epa! Tive um problema na hora de ler o conteúdo do arquivo de entrada. Mostra isso aqui pro gostoso do seu namorado resolver: \n {}",e
-    ))
-}
-
-fn write_result(result: impl Into<String>) -> Result<(), String> {
-    fs::write(OUTPUT_FILE, result.into()).map_err(|err| format!("Epa! Não consegui escrever o arquivo de saída! Mostra isso aqui pro gostoso do seu namorado: \n {}", err))
 }
